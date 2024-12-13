774c2bf4a9cad75aa4e9a34b707cd7185bfeaf90
Azamkhon <166040135+azamkhon590@users.noreply.github.com>
2024-12-13 21:34:50 +0300

    back_6

--- vue/src/views/NewsListView.vue
+++ vue/src/views/NewsListView.vue
@@ -1,23 +1,23 @@
 <script setup>
-    import { ref, onMounted } from 'vue';
-    import axios from 'axios';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
-    const newsList = ref([]);
-    const categoryList = ref([]);
+const newsList = ref([]);
+const categoryList = ref([]);
 
-    onMounted(async () => {
-        try{
-            const response = await axios.get("http://127.0.0.1:8000/api/news");
-            newsList.value = response.data.data;
+onMounted(async () => {
+    try {
+        const response = await axios.get("http://127.0.0.1:8000/api/news");
+        newsList.value = response.data.data;
 
-            const category_response = await axios.get("http://127.0.0.1:8000/api/category");
-            categoryList.value = category_response.data.data;
+        const category_response = await axios.get("http://127.0.0.1:8000/api/category");
+        categoryList.value = category_response.data.data;
 
-            console.log(newsList.value);
-        } catch {
+        console.log(newsList.value);
+    } catch {
 
-        }
-    });
+    }
+});
 </script>
 
 <template>
@@ -26,6 +26,14 @@
     </div>
     <div class="logo">
         <h1 class="text-center">News</h1>
+    </div>
+    <div class="searhe">
+        <div class="input-group mb-3 p-3">
+            <div class="form-floating">
+                <input type="text" class="form-control" id="floatingInputGroup1" placeholder="Searhe">
+                <label for="floatingInputGroup1">Searhe</label>
+            </div>
+        </div>
     </div>
     <div class="row">
         <div v-if="newsList.length <= 0">Empty</div>
@@ -36,7 +44,8 @@
                     <h5 class="card-title">{{ news.title }}</h5>
                     <p class="card-text">{{ news.short_content }}</p>
                     <p class="card-text link-danger">{{ news.date }}</p>
-                    <router-link class="btn btn-primary" :to="{name: 'news_single', params: {id: news.id}}">Подробнее</router-link>
+                    <router-link class="btn btn-primary"
+                        :to="{ name: 'news_single', params: { id: news.id } }">Подробнее</router-link>
                 </div>
             </div>
         </div>
